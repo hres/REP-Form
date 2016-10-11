06d9c4cb6964faf1e17782d4bff84dba0fcc6dc6
dkiltyhc <dan.kilty@canada.ca>
2016-10-11 10:28:01 -0400

    New dossier files, missed adding

---
+++ dossier/app/components/country-list/cmp-country-list.js
@@ -0,0 +1,124 @@
+/**
+ * Created by Abdessamad on 8/16/2016.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('countryListModule', ['dataLists'])
+})();
+
+(function () {
+    'use strict';
+
+    angular
+        .module('countryListModule')
+        .component('cmpCountryList', {
+            templateUrl: './app/components/country-list/tpl-country-list.html',
+            controller: countryListCtrl,
+            bindings: {
+                withUnknown:'<',
+                listItems: '<',
+                onUpdate: '&',
+                onDelete: '&'
+            }
+        });
+
+    countryListCtrl.$inject = ['$filter','getCountriesISO3166'];
+
+
+    function countryListCtrl($filter, getCountriesISO3166){
+        var self = this;
+
+        self.$onInit = function(){
+
+            var _countries = self.withUnknown?["UNKNOWN"].concat(getCountriesISO3166.getCountryList3Letter()):getCountriesISO3166.getCountryList3Letter();
+            self.model={
+                countries : _countries,
+                list : [
+                    {"id":1, "name":"ARG"},
+                    {"id":2, "name":"CAN"},
+                    {"id":3, "name":"USA"}
+                ],
+                selected:{}
+
+
+            }
+
+            if(self.listItems){
+                self.model.list = self.listItems;
+            }
+        }
+
+        // gets the template to ng-include for a table row / item
+        self.getTemplate = function (item) {
+            if (item.id === self.model.selected.id) return 'edit';
+            else return 'display';
+        };
+
+        self.addNew = function(){
+
+            var maxID = getListMaxID();
+
+            //console.log("addNew maxID: " + JSON.stringify(maxID) );
+
+            var item = {"id":maxID + 1, "name":""};
+
+            self.model.list.push(item);
+            self.editRecord(item);
+
+        };
+
+        self.editRecord = function (item) {
+            self.model.selected = item;
+        };
+
+        self.saveRecord = function (_id) {
+           // console.log("Saving item: "+_id);
+            var idx = self.model.list.indexOf(
+                $filter('filter')(self.model.list, {id: _id}, true)[0]
+            );
+            self.model.list[idx] = self.model.selected;
+            self.reset();
+        };
+
+        self.deleteRecord = function (_id) {
+            //console.log("Deleting item: "+_id);
+
+            var idx = self.model.list.indexOf(
+                $filter('filter')(self.model.list, {id: _id}, true)[0]
+            );
+            if(idx < 0) return;
+
+            self.model.list.splice(idx,1);
+        };
+
+
+
+        self.reset = function () {
+            var item = self.model.selected;
+            //console.log('reset selected: ' + item.toSource());
+            if(angular.isUndefined(item))
+                return;
+
+            //self.deleteRecord(item.id)
+            self.model.selected = {};
+
+        };
+
+        function getListMaxID(){
+
+            var out = 0;
+            var list = self.model.list;
+            if (list) {
+                for (var i = 0; i<list.length; i++) {
+                    if (list[i].id > out) {
+                        out = list[i].id;
+                    }
+                }
+            }
+            return out;
+
+        }
+    }
+})();
---
+++ dossier/app/components/expandingTable/cmp-expanding-table.js
@@ -0,0 +1,174 @@
+/**
+ * Created by dkilty on 8/6/2016.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('expandingTable', [])
+})();
+
+(function () {
+    'use strict';
+
+    angular
+        .module('expandingTable')
+        .component('cmpExpandingTable', {
+            templateUrl: './app/components/expandingTable/tpl-expanding-table.html',
+            controller: expandingTableCtrl,
+            controllerAs: 'expandTblCtrl',
+            transclude:true,
+            bindings: {
+                title: '@', /*deprecated*/
+                listItems: '<',
+                columnDef:'<',
+                disableSelection:'<',
+                selectRecord: '<',
+                resetToCollapsed: '<'
+            }
+        });
+    expandingTableCtrl.$inject = ['$filter']
+    function expandingTableCtrl($filter) {
+        var vm = this;
+        vm.focused = false;
+        vm.disableExpand=false;
+        vm.tableRowExpanded = false;
+        vm.tableRowIndexCurrExpanded = "";
+        vm.tableRowIndexPrevExpanded = "";
+        vm.numberCols=_getNumberKeys(vm.columnDef)
+        vm.dayDataCollapse = _createArray(vm.listItems.length, true);
+
+        vm.$onInit = function () {
+        }
+
+        vm.getExpandedState = function (row) {
+            if (row === vm.tableRowIndexCurrExpanded) {
+                return true;
+            }
+            return false
+        }
+
+        vm.$onChanges = function (changes) {
+
+            if (changes.listItems) {
+                vm.dayDataCollapse = _createArray(vm.listItems.length, true);
+                vm.resetTableRow();
+            }
+
+            /**
+             Resets the table to collapsed. Note this  is not a true reset
+             If a valid index is selected, toggles (Expands) the row after a reset.
+             This allows expanding the same row index after a row has been added at the beginning
+             For a true reset, first set select record to -1
+             */
+            if(changes.resetToCollapsed){
+                if(changes.resetToCollapsed.currentValue){
+                    vm.resetTableRow();
+                    if (!changes.selectRecord) {
+                        updateTableRow(vm.selectRecord);
+                    }
+                }
+            }
+            if(changes.selectRecord){
+                var selectIndex=parseInt(changes.selectRecord.currentValue);
+                if(selectIndex>=0) {
+                    vm.selectTableRow(selectIndex);
+                }else{
+                    vm.resetTableRow()
+                }
+            }
+            if(changes.disableSelection){
+                vm.disableExpand=changes.disableSelection.currentValue;
+            }
+        }
+        function updateTableRow(textIndex) {
+            var selectIndex = parseInt(textIndex);
+            if (selectIndex >= 0) {
+                vm.selectTableRow(selectIndex);
+            }
+
+        }
+
+
+        /**
+         * Utility function for determining the number of columns to create
+         * @param myobj
+         * @returns {number}
+         * @private
+         */
+        function _getNumberKeys(myobj){
+            var count = 0;
+            for (var k in myobj) {
+                if (myobj.hasOwnProperty(k)) count++;
+            }
+            return count;
+        }
+
+        /**
+         * Creates an array of the specified size and default values
+         * @param arraySize
+         * @param initialVal
+         * @returns {Array}
+         * @private
+         */
+         function _createArray(arraySize,initialVal){
+         var anArray = [];
+         for (var i = 0; i < arraySize; i++) anArray.push (initialVal);
+         return anArray
+         }
+
+        vm.deletedRow=function(){
+            vm.tableRowExpanded = false;
+            vm.tableRowIndexCurrExpanded = "";
+        }
+        /**
+         * @ngdoc resets the table to a collapsed state
+         */
+        vm.resetTableRow = function () {
+            vm.tableRowIndexPrevExpanded = "";
+            vm.tableRowExpanded = false;
+            vm.tableRowIndexCurrExpanded = "";
+            vm.dayDataCollapse = _createArray(vm.listItems.length, true);
+
+        }
+
+        vm.dayDataCollapseFn = function () {
+            for (var i = 0; vm.listItems.length - 1; i += 1) {
+                vm.dayDataCollapse.append('true');
+            }
+        };
+        vm.selectTableRow = function (index) {
+            //if selection
+         if (vm.disableExpand) return;
+            if (vm.dayDataCollapse === 'undefined') {
+                vm.dayDataCollapse = vm.dayDataCollapseFn();
+            } else {
+
+                if (vm.tableRowExpanded === false && vm.tableRowIndexCurrExpanded === "") {
+                    vm.tableRowIndexPrevExpanded = "";
+                    vm.tableRowExpanded = true;
+                    vm.tableRowIndexCurrExpanded = index;
+                    // vm.storeIdExpanded = storeId;
+                    vm.dayDataCollapse[index] = false;
+                } else if (vm.tableRowExpanded === true) {
+                    if (vm.tableRowIndexCurrExpanded === index) {
+                        vm.tableRowExpanded = false;
+                        vm.tableRowIndexCurrExpanded = "";
+                        vm.dayDataCollapse[index] = true;
+                    } else {
+                        vm.tableRowIndexPrevExpanded = vm.tableRowIndexCurrExpanded;
+                        vm.tableRowIndexCurrExpanded = index;
+                        //  vm.storeIdExpanded = storeId;
+                        vm.dayDataCollapse[vm.tableRowIndexPrevExpanded] = true;
+                        vm.dayDataCollapse[vm.tableRowIndexCurrExpanded] = false;
+                    }
+                }
+            }
+        }
+
+
+    }
+
+
+
+})();
