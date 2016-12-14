8904b2cc9507db110137ef873c7135c36eea4a69
Dan Kilty <dan.kilty@canada.ca>
2016-12-14 14:32:29 -0500

    Minor dossier bug fixes,. Disable XML button. Strength as a number

---
+++ dossier/app/components/repContactList/cmp-rep-contact-list_20161214_1424.js
@@ -0,0 +1,165 @@
+/**
+ * Created by dkilty on 8/6/2016.
+ */
+
+
+(function () {
+    'use strict';
+
+    angular
+        .module('contactModule26', ['contactModule25', 'expandingTable', 'repContactService'])
+})();
+
+(function () {
+    'use strict';
+
+    angular
+        .module('contactModule26')
+        .component('cmpRepContactList', {
+            templateUrl: './app/components/repContactList/tpl-rep-contact-list_20161214_1424.html',
+            controller: contactListCtrl,
+            controllerAs: 'contactListCtrl',
+            bindings: {
+                contacts: '<',
+                //  onUpdate: '&',
+                // getNewContact: '&',
+                showListErrors: '&',
+                parentDirty: '<',
+                isAmend: '<'
+            }
+        });
+    contactListCtrl.$inject = ['$filter', 'RepContactService']
+    function contactListCtrl($filter, RepContactService) {
+        var vm = this;
+        vm.selectRecord = -1; //the record to select
+        vm.isDetailValid = true; //used to track if details valid. If they are  not do not allow expander collapse
+        vm.contactList = [];
+        vm.oneRecord = ""; //using required as the validaiton
+        vm.isParentDirty = false; //tracks whether the parent form has been dirtied
+        vm.formAmend = false;
+        vm.resetCollapsed = false;
+        var repContactService = new RepContactService();
+        vm.columnDef = [
+            {
+                label: "FIRST_NAME",
+                binding: "givenName",
+                width: "40"
+            },
+
+            {
+                label: "LAST_NAME",
+                binding: "surname",
+                width: "40"
+            },
+            {
+                label: "ONE_ROLE",
+                binding: "repRole",
+                width: "20"
+            }
+        ]
+        /**
+         * using to get contact list
+         */
+        vm.$onInit = function () {
+            vm.focused = false;
+            // vm.contactList = vm.contacts;
+        }
+        vm.$onChanges = function (changes) {
+            if (changes.contacts) {
+                vm.contactList = changes.contacts.currentValue;
+                vm.updateErrorState();
+            }
+            if (changes.parentDirty) {
+                vm.isParentDirty = changes.parentDirty.currentValue;
+            }
+            if (changes.isAmend) {
+                vm.formAmend = changes.isAmend.currentValue;
+            }
+
+        }
+        vm.isAddContact = function () {
+            if (vm.contactList.length > 1) {
+
+                return false;
+            }
+            return (vm.isDetailValid);
+        }
+
+        vm.showNoRecordError = function (isInvalid) {
+            return ((vm.isParentDirty && isInvalid  ) || (vm.showListErrors() && isInvalid));
+        }
+
+        vm.setValid = function (value) {
+            vm.isDetailValid = value; //this is a shared value
+        }
+
+        vm.showError = function () {
+
+            return (vm.contactListForm.$invalid && !vm.contactListForm.$pristine) || (vm.contactListForm.$invalid && vm.showListErrors());
+        }
+
+        vm.onUpdateContactRecord = function (record) {
+            var idx = vm.contactList.indexOf(
+                $filter('filter')(vm.contactList, {repRole: record.repRole}, true)[0]
+            ); //TODO fix filter
+            vm.contactList[idx] = angular.copy(record);
+            vm.updateErrorState();
+            vm.contactListForm.$setPristine();
+            vm.resetCollapsed = !vm.resetCollapsed;
+            vm.disableAdd();
+        }
+        /***
+         * Tracks if no records
+         */
+        vm.updateErrorState = function () {
+            if (vm.contactList && vm.contactList.length > 0) {
+                vm.oneRecord = "is value";
+            } else {
+                vm.oneRecord = "";
+            }
+
+        }
+        vm.deleteContact = function (cID) {
+            var idx = vm.contactList.indexOf(
+                $filter('filter')(vm.contactList, {repRole: cID}, true)[0]
+            );
+            vm.contactList.splice(idx, 1);
+            //check if only one record
+            //todo get Alternate
+            if (vm.contactList.length === 1 && vm.contactList[0].repRole !== "PRIMARY") {
+                vm.contactList[0].repRole = "PRIMARY"
+                var temp=angular.copy(vm.contactList);
+                vm.contactList=[]
+                vm.contactList=temp;
+            }
+
+            //vm.onUpdate({newList: vm.contactList});
+            vm.updateErrorState();
+            vm.disableAdd()
+            vm.setValid(true);
+            vm.selectRecord = -1;
+            vm.resetCollapsed = !vm.resetCollapsed;
+        }
+        /**
+         * Adds a contact to the contact list
+         */
+        vm.addContact = function () {
+            var defaultContact = repContactService.createRepContact(vm.contactList);
+            vm.contactList.push(defaultContact);
+            //select table row first then make invalid
+            vm.selectRecord = (vm.contactList.length - 1);
+            vm.setValid(false);
+        }
+
+        vm.disableAdd = function () {
+            if (!vm.contactList) return false;
+            var isInvalid = !vm.isDetailValid || vm.contactList.length == 2 || (vm.contactList.length > 0 && vm.contactListForm.$invalid)
+               // || (vm.contactListForm.$valid && vm.contactListForm.$dirty);
+
+            return isInvalid;
+
+        }
+
+    }
+
+})();
---
+++ dossier/app/components/tabs/cmp-tabs_20161214_1424.js
@@ -0,0 +1,79 @@
+/**
+ * Created by Abdessamad on 7/24/2016.
+ */
+(function () {
+    'use strict';
+
+
+    //TODO: Lazy load modules
+    angular.module('tabsModule', ['formulationsModule', 'appendixFourModule']);
+})();
+
+(function () {
+    'use strict';
+    angular.module('tabsModule').component('cmpTabs', {
+        templateUrl: './app/components/tabs/tpl-tabs_20161214_1424.html',
+        controller: tabsCtrl,
+        controllerAs: 'tabsCtrl',
+        bindings: {
+            formulationList : '<',
+            appendix4List : '<',
+            recordChanged: '&',
+            service: '<'
+        }
+    });
+
+    tabsCtrl.$inject = ['$scope'];
+
+
+    function tabsCtrl($scope) {
+
+        var self = this;
+        self.tabs = [
+            {
+                label: "FORMULATIONS",
+                selected: true,
+                disabled: false,
+                errors: true,
+                form: {}
+            },
+            {
+                label: "APPENDIX4",
+                selected: false,
+                disabled: false,
+                errors: false,
+                form: {}
+            }
+        ];
+        self.$onInit = function () {
+
+            //console.log(self.tabs[0].form.$invalid)
+        };
+        self.$onChanges = function () {
+            /*  if(changes.service){
+
+             self.dosService=changes.service.currentValue;
+             }*/
+
+        }
+
+        self.selectTab = function (idx) {
+
+            /*  angular.forEach(self.tabs, function (tab) {
+                    tab.selected = false;
+             tab.errors=tab.form.$invalid;
+             });*/
+
+            for (var i = 0; i < self.tabs.length; i++) {
+                self.tabs[i].selected = false;
+                if (idx !== i) {
+                    self.tabs[i].errors = self.tabs[i].form.$invalid;
+                }
+            }
+
+                self.tabs[idx].selected = true;
+            //self.tabs[idx].errors= self.tabs[idx].form.$invalid
+        };
+    }
+
+})();
