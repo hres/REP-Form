db8616a975566c39089b8d53180cb80cb5658b20
Dan Kilty <dan.kilty@canada.ca>
2019-01-15 12:01:51 -0500

    temp fixes

--- mdi/dep2/request.js
+++ mdi/dep2/request.js
@@ -26,12 +26,13 @@ $(document).ready(() => {
     //remove brackets
     for(let i=0;i<q.length;i++){
         let _q=q[i];
-        if (_q.indexOf("(") > -1 || _q.indexOf(")") > -1){
+        if (_q.indexOf("(") > -1 || _q.indexOf(")") > -1 ||q.length==0){
             q.splice(q.indexOf(_q), 1);
             i=i-1; //dont increment
         }
     }
 
+    console.log("qis "+q);
 
     $("#terms").text(q.join(" "));
     console.log(q);
@@ -46,14 +47,20 @@ $(document).ready(() => {
 
 function requestDocuments(q) {
 
-    var url = documentURL + "?select=incident.incident_id";
+    var url = documentURL;
+    if(q[0].length>0){ //TODO hacks
+       //https://rest-dev.hres.ca/mdi/mdi_search?&offset=0&limit=25
+        console.log("greater  than zero")
+        url = documentURL + "?select=incident.incident_id";
+        q.forEach((_q) => {
+            //https://rest-dev.hres.ca/mdi/mdi_search?select=incident.incident_id&search=fts.123
+            //https://rest-dev.hres.ca/mdi/mdi_search?select=incident.incident_id&search=fts.onetouch&search=fts.ultra&search=fts.blood&offset=0&limit=25
+            url += ("&search=fts." + _q);
+        });
+    }else{
+        url = documentURL + "?";
+    }
 
-    q.forEach((_q) => {
-        console.log(_q)
-        //https://rest-dev.hres.ca/mdi/mdi_search?select=incident.incident_id&search=fts.123
-        //https://rest-dev.hres.ca/mdi/mdi_search?select=incident.incident_id&search=fts.onetouch&search=fts.ultra&search=fts.blood&offset=0&limit=25
-        url += ("&search=fts." + _q);
-    });
     url+="&offset="+page;
     url+="&limit="+limit;
     console.log(url)
@@ -213,3 +220,39 @@ function makeDate(iso) {
 
     return d.getFullYear() + "-" + month + "-" + day;
 }
+function ExportTableToCSV($table, filename) {
+
+    var $rows = $table.find('tr:has(td),tr:has(th)'),
+        tmpColDelim = String.fromCharCode(11), // vertical tab character
+        tmpRowDelim = String.fromCharCode(0), // null character
+
+        // actual delimiter characters for CSV format
+        colDelim = '","',
+        rowDelim = '"\r\n"',
+
+        // Grab text from table into CSV formatted string
+        csv = '"' + $rows.map(function (i, row) {
+            var $row = $(row), $cols = $row.find('td,th');
+
+            return $cols.map(function (j, col) {
+                var $col = $(col), text = $col.text();
+                return text.replace(/"/g, '""'); // escape double quotes
+
+            }).get().join(tmpColDelim);
+
+        }).get().join(tmpRowDelim)
+            .split(tmpRowDelim).join(rowDelim)
+            .split(tmpColDelim).join(colDelim) + '"',
+        // Data URI
+        csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+    //console.log(csv);
+
+    if (window.navigator.msSaveBlob) { // IE 10+
+        //alert('IE' + csv);
+        window.navigator.msSaveOrOpenBlob(new Blob([csv], { type: "text/plain;charset=utf-8;" }), filename)
+    }
+    else {
+        $(this).attr({ 'download': filename, 'href': csvData, 'target': '_blank' });
+    }
+}
+
