01d096ad4f8600a3a25cc255f9f48de709527f0d
stevezhaoca <steve.zhao@canada.ca>
2019-03-12 16:25:31 -0400

    pilot Production release - MDS forms

---
+++ mds/company/en/assets/js/rep.js
@@ -0,0 +1,15 @@
+
+function currentDate() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  return today = yyyy + '-' + mm + '-' + dd;
+}
